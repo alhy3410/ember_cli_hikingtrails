ca514d19636fd8ee5edce670643c2daa14c9765b
Andrew <student@epicodus.com>
2015-06-17 16:53:47 -0700

    removes duplicates of each new comment added

--- app/controllers/comments.js
+++ app/controllers/comments.js
@@ -4,17 +4,23 @@ var CommentsController = {
   needs: ['hike'],
   actions: {
     save: function() {
+      var hike = this.get('controllers.hike.model');
       var newAnswer = this.store.createRecord('comment', {
         author: this.get('author'),
         title: this.get('title'),
         answer: this.get('answer')
       });
-      newAnswer.save();
 
-      var hike = this.get('controllers.hike.model');
-      hike.get('comments').pushObject(newAnswer);
-      hike.save();
+      newAnswer.save().then(function(){
+        hike.get('comments').pushObject(newAnswer);
+        hike.save();
+      })
 
+      this.setProperties({
+        author: "",
+        author: "",
+        author: ""
+      })
       this.transitionToRoute('hike', hike.id);
     }
   }
